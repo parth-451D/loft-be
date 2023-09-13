27c08a8363f3698ee4f85b9f211e823310a3891d
parth-451D <parth.dalsaniya2001@gmail.com0>
2023-09-13 23:38:07 +0530

    api added of floor availability

--- routes/flatModule/flatController.js
+++ routes/flatModule/flatController.js
@@ -11,7 +11,6 @@ const checkForMinimunStay = async (req, res, next) => {
       "SELECT * FROM minimum_stay WHERE ? BETWEEN start_date AND end_date;",
       [req.body.start_date]
     );
-    console.log("rows", rows.length);
     if (rows.length > 0) {
       // now check for days of that range
       const startDate = moment(req.body.start_date);
@@ -30,6 +29,7 @@ const checkForMinimunStay = async (req, res, next) => {
         next();
       } else {
         return res.send({
+          code: 401,
           message: `Number of days requested is less then minimum stay days, Kindly select more then ${
             rows[0].selected_days - 1
           } days`,
@@ -37,69 +37,9 @@ const checkForMinimunStay = async (req, res, next) => {
       }
     }
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ code: 400, message: error.message });
   }
 };
-
-// const checkForAvailableFloor = async (req, res, next) => {
-//   const [bookedRows, bookedFields] = await pool.query(
-//     "SELECT unitNo FROM temp_booking WHERE ? BETWEEN start_date AND end_date;",
-//     [req.body.start_date]
-//   );
-//   const bookedFlats = bookedRows.map((ele) => {
-//     return ele.unitNo;
-//   });
-//   const flatArray = [
-//     [101, 102, 103, 104, 105, 106, 107, 108],
-//     [201, 202, 203, 204, 205, 206, 207, 208],
-//     [301, 302, 303, 304, 305, 306, 307, 308],
-//   ];
-//   const filteredFlatArray = flatArray.map((subArray) =>
-//     subArray.filter((flat) => !bookedFlats.includes(flat))
-//   );
-//   const [maintenanceRows, maintenanceFields] = await pool.query(
-//     "SELECT * from maintenance join flats on maintenance.flatId = flats.id where ? BETWEEN start_date AND end_date;",
-//     [req.body.start_date]
-//   );
-//   const maintenanceFlats = maintenanceRows.map((ele) => {
-//     return ele.unitNo;
-//   });
-//   const finalArray = filteredFlatArray.map((subArray) => {
-//     return subArray.filter((flat) => {
-//       return !maintenanceFlats.includes(flat);
-//     });
-//   });
-
-//   let response = [
-//     { floor: 1, clickable: true },
-//     { floor: 2, clickable: true },
-//     { floor: 3, clickable: true },
-//   ];
-
-//   finalArray.map((ele, idx) => {
-//     if (ele.length > 0) {
-//       response[idx].clickable = false;
-//     }
-//   });
-//   const matchingObjects = [];
-//   if(req.body.bathroom ){
-//     finalArray.forEach((subArray) => {
-//       subArray.forEach((unitNo) => {
-//         console.log(unitNo,"ssss");
-//         const matchingObject = maintenanceRows.find((obj) => obj.unitNo === unitNo);
-// console.log(maintenanceRows,"ddddddd")
-//         console.log(matchingObject,"match")
-//         if (matchingObject && matchingObject.bathrooms >= req.body.bathroom) {
-//           console.log("bathroom inp",req.body.bathroom)
-//           matchingObjects.push(matchingObject);
-//         }
-//       });
-//     });
-//   }
-
-//   console.log(matchingObjects,"match")
-
-// };
 
 const checkForAvailableFloor = async (req, res) => {
   const [bookedRows, bookedFields] = await pool.query(
@@ -126,66 +66,41 @@ const checkForAvailableFloor = async (req, res) => {
   const filteredFlatArray = flatArray.map((subArray) =>
     subArray.filter((flat) => !notAvailableFlats.includes(flat))
   );
-  // const [allFlatsRows, allFlatsFields] = await pool.query(
-  //   "SELECT temp_booking.*, flats.bathrooms, flats.beds from temp_booking join flats on flats.unitNo = temp_booking.unitNo"
-  // );
 
-  // console.log("allFlatsRows", allFlatsRows);
-  console.log("filteredFlatArray", filteredFlatArray);
-
-  // const filteredFlatArrayWithCriteria = filteredFlatArray.map(async(subArray) =>
-  //   subArray.filter(async (unitNo) => {
-  //     const [flatRow, flatField] = await pool.query(
-  //       "select * from flats where unitNo = ?", [unitNo]
-  //     )
-  //     // Find the matching maintenance object for the unitNo
-  //     // const matchingMaintenanceObject = allFlatsRows.find((obj) => {
-  //     //   console.log("object", obj);
-  //     //   return obj.unitNo === unitNo
-  //     // });
-  //     // console.log("flatRow", flatRow)
-  //     console.log("bathrooms", flatRow[0].bathrooms);
-  //     console.log("beds", flatRow[0].beds)
-  //     return (flatRow[0].bathrooms >= +req.body.bathrooms && flatRow[0].beds >= +req.body.beds)
-  //     // console.log("matchingMaintenanceObject", matchingMaintenanceObject)
-  //     // // Check if the unit meets the criteria
-  //     // return (
-  //     //   matchingMaintenanceObject &&
-  //     //   matchingMaintenanceObject.bathrooms >= req.body.bathrooms &&
-  //     //   matchingMaintenanceObject.beds >= req.body.bedrooms
-  //     // );
-  //   })
-  // );
-
-  const filteredFlatArrayWithCriteria = await Promise.all(
-    filteredFlatArray.map(async (subArray) => {
-      const filteredUnits = await Promise.all(
-        subArray.map(async (unitNo) => {
-          console.log("unitNo", unitNo);
-          const [flatRow, flatField] = await pool.query(
-            "SELECT * FROM flats WHERE unitNo = ?",
-            [unitNo]
-          );
-          console.log("Row data", flatRow);
-          // Check if the query returned any rows and if the unit meets the criteria
-          if (
-            flatRow.length > 0 &&
-            flatRow[0].bathrooms >= req.body.bathrooms &&
-            flatRow[0].beds >= req.body.beds
-          ) {
-            return unitNo; // Return the unitNo if it meets the criteria
-          }
-          return null; // Return null if the unit is not found or doesn't meet the criteria
-        })
-      );
-
-      console.log("filter units", filteredUnits);
-
-      // Remove null values (units that didn't meet the criteria or weren't found)
-      return filteredUnits.filter((unitNo) => unitNo !== null);
-    })
+  const [flatsWithCriteriaRows, flatsWithCriteriaFields] = await pool.query(
+    "select unitNo from flats where bathrooms >= ? and beds >= ?",
+    [req.body.bathrooms, req.body.beds]
   );
-  console.log("sdfsfsgegdv", filteredFlatArrayWithCriteria);
+
+  const criteriaFlats = flatsWithCriteriaRows.map((ele) => {
+    return ele.unitNo;
+  });
+
+  const finalArray = filteredFlatArray.map((subarray) =>
+    subarray.filter((item) => criteriaFlats.includes(item))
+  );
+  console.log("final array ", finalArray);
+  const response = {
+    floor1: finalArray[0].length > 0 ? true : false,
+    floor2: finalArray[1].length > 0 ? true : false,
+    floor3: finalArray[2].length > 0 ? true : false,
+  };
+
+  res
+    .status(200)
+    .json({
+      code: 200,
+      message: "Floor data get successfully",
+      data: response,
+    });
 };
 
-module.exports = { checkForMinimunStay, checkForAvailableFloor };
+const checkForAvailableFlats = async (req, res) => {
+   const [bookedFlatsRows, bookedFlatsFields] = await pool.query(
+    "select * from temp_booking join flats on flats.unitNo = temp_booking.unitNo where ? between start_date and end_date and floor = ? ", [req.body.start_date, req.body.floor]
+   ) 
+   
+   console.log("bookedFlatsRows", bookedFlatsRows)
+};
+
+module.exports = { checkForMinimunStay, checkForAvailableFloor, checkForAvailableFlats };
--- routes/flatModule/flatRoute.js
+++ routes/flatModule/flatRoute.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { checkForMinimunStay, checkForAvailableFloor } = require("./flatController");
+const {
+  checkForMinimunStay,
+  checkForAvailableFloor,
+  checkForAvailableFlats,
+} = require("./flatController");
 
 router.get("/available-flats", checkForMinimunStay);
 
-router.post("/check-available-floors", checkForAvailableFloor);
+// get available floors by criteria
+router.post(
+  "/check-available-floors",
+  checkForMinimunStay,
+  checkForAvailableFloor
+);
 
+// get available flats 
+router.post("/check-available-flats", checkForMinimunStay, checkForAvailableFlats)
 module.exports = router;
